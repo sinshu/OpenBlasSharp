e27cc69b1bf54b0627795c3e03002dfee50cce37
Nobuaki Tanaka <sinshu.d@gmail.com>
2024-01-22 16:04:05 +0900

    Add OpenBLAS specific functions.

--- OpenBlasSharp/OpenBlas.cs
+++ OpenBlasSharp/OpenBlas.cs
@@ -1,4 +1,7 @@
-﻿namespace OpenBlasSharp
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace OpenBlasSharp
 {
     /// <summary>
     /// Provides OpenBLAS functions.
@@ -9,5 +12,50 @@
         /// The name of the OpenBLAS library.
         /// </summary>
         public const string LibraryName = "libopenblas";
+
+        /// <summary>
+        /// Set the number of threads on runtime.
+        /// </summary>
+        /// <param name="numThreads">The number of threads.</param>
+        [DllImport(LibraryName, EntryPoint = "openblas_set_num_threads", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void SetNumThreads(int numThreads);
+
+        /// <summary>
+        /// Get the number of threads on runtime.
+        /// </summary>
+        /// <returns>The number of threads.</returns>
+        [DllImport(LibraryName, EntryPoint = "openblas_get_num_threads", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int GetNumThreads();
+
+        /// <summary>
+        /// Get the number of physical processors (cores).
+        /// </summary>
+        /// <returns>The number of processors.</returns>
+        [DllImport(LibraryName, EntryPoint = "openblas_get_num_procs", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int GetNumProcs();
+
+        /// <summary>
+        /// Get the build configure on runtime.
+        /// </summary>
+        /// <returns>The build configure.</returns>
+        public static string GetConfig()
+        {
+            return Marshal.PtrToStringAnsi(GetConfigCore());
+        }
+
+        /// <summary>
+        /// Get the CPU corename on runtime.
+        /// </summary>
+        /// <returns>The CPU corename.</returns>
+        public static string GetCorename()
+        {
+            return Marshal.PtrToStringAnsi(GetCorenameCore());
+        }
+
+        [DllImport(LibraryName, EntryPoint = "openblas_get_config", CallingConvention = CallingConvention.Cdecl)]
+        private static extern IntPtr GetConfigCore();
+
+        [DllImport(LibraryName, EntryPoint = "openblas_get_corename", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        private static extern IntPtr GetCorenameCore();
     }
 }
---
+++ OpenBlasSharpTest/OpenBlasFunctions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using NUnit.Framework;
+using OpenBlasSharp;
+
+namespace OpenBlasSharpTest
+{
+    public class OpenBlasFunctions
+    {
+        [Test]
+        public void SetNumThreads()
+        {
+            OpenBlas.SetNumThreads(1);
+        }
+
+        [Test]
+        public void GetNumThreads()
+        {
+            var value = OpenBlas.GetNumThreads();
+            Console.WriteLine(value);
+        }
+
+        [Test]
+        public void GetNumProcs()
+        {
+            var value = OpenBlas.GetNumProcs();
+            Console.WriteLine(value);
+        }
+
+        [Test]
+        public void GetConfig()
+        {
+            var value = OpenBlas.GetConfig();
+            Console.WriteLine(value);
+        }
+
+        [Test]
+        public void GetCorename()
+        {
+            var value = OpenBlas.GetCorename();
+            Console.WriteLine(value);
+        }
+    }
+}
