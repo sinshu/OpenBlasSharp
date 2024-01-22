dabf6e595ca2a3820444110dc79be958de947b5b
Nobuaki Tanaka <sinshu.d@gmail.com>
2024-01-22 14:57:31 +0900

    Support .NET Standard 2.0.

---
+++ clean.bat
@@ -0,0 +1,7 @@
+rmdir /s /q "CodeGenerator\bin"
+rmdir /s /q "CodeGenerator\obj"
+rmdir /s /q "OpenBlasSharp\bin"
+rmdir /s /q "OpenBlasSharp\obj"
+rmdir /s /q "OpenBlasSharpTest\bin"
+rmdir /s /q "OpenBlasSharpTest\obj"
+pause
